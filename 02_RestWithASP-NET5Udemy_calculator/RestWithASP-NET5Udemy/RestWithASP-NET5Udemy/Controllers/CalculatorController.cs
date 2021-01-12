ef0876aba8f050333c46c4094383f9144c3f643c
vitorcsharp@hotmail.com <vitorcsharp@hotmail.com>
2021-01-12 17:08:33 -0300

    update calculator

--- 02_RestWithASP-NET5Udemy_calculator/RestWithASP-NET5Udemy/RestWithASP-NET5Udemy/Controllers/CalculatorController.cs
+++ 02_RestWithASP-NET5Udemy_calculator/RestWithASP-NET5Udemy/RestWithASP-NET5Udemy/Controllers/CalculatorController.cs
@@ -9,7 +9,7 @@ namespace RestWithASP_NET5Udemy.Controllers
 {
     //para mudar a url ao abrir e não mostrar WeattherForecats va em properties"LaunchSettings.json" e alteres os "launchUrl", altere tambem o "sslPort" de 44396 para 44300
     
-
+    //==========================================================
 
     [ApiController]
     [Route("[controller]")]
@@ -23,14 +23,25 @@ namespace RestWithASP_NET5Udemy.Controllers
         //    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
         //};
 
+        //==========================================================
+
         private readonly ILogger<CalculatorController> _logger;
+
+
+        //==========================================================
+
 
         public CalculatorController(ILogger<CalculatorController> logger)
         {
             _logger = logger;
         }
 
+        //calculadora pelo get
+        //==========================================================
+        //aqui sera a adição
+
         //quando passar a requisição para o "firstNumber" e "secondNumber" ele verifica se é numerico(se for string da erro)
+        //esse codio abaixo é o "endPoint"
         [HttpGet("sum/{firstNumber}/{secondNumber}")] // antes aqui estava assim [HttpGet]
 
         // dentro do Get abaixo coloque o string firstNunber e string secondNunber (poderia ao inves de string ser INT)
@@ -42,21 +53,98 @@ namespace RestWithASP_NET5Udemy.Controllers
                 //precisa ser convertido pois esta como string
 
                 return Ok(sum.ToString());
+
+                //apague essa implementação abaixo
+                //var rng = new Random();
+                //return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+                //{
+                //    Date = DateTime.Now.AddDays(index),
+                //    TemperatureC = rng.Next(-20, 55),
+                //    Summary = Summaries[rng.Next(Summaries.Length)]
+                //})
+                //.ToArray();
+
+
+
             }
             return BadRequest("Invalid Input"); //retornar quand o input vier errado (vier como string e não numerico)
 
 
-
-            //apague essa implementação abaixo
-            //var rng = new Random();
-            //return Enumerable.Range(1, 5).Select(index => new WeatherForecast
-            //{
-            //    Date = DateTime.Now.AddDays(index),
-            //    TemperatureC = rng.Next(-20, 55),
-            //    Summary = Summaries[rng.Next(Summaries.Length)]
-            //})
-            //.ToArray();
         }
+
+        //==========================================================
+        //realizando outras contas matematicas
+
+        //aqui sera a raiz quadrada
+        [HttpGet("square-root/{firstNumber}")] 
+        
+        public IActionResult Squareroot(string firstNumber)
+        {
+            if (IsNumeric(firstNumber))
+            {
+                var squareroot = Math.Sqrt((double)ConvertToDecimal(firstNumber)); 
+
+                return Ok(squareroot.ToString());
+            }
+            return BadRequest("Invalid Input"); 
+
+
+        }
+
+
+        //==========================================================
+
+        [HttpGet("Division/{firstNumber}/{secondNumber}")] 
+
+        //aqui sera a divisao
+        public IActionResult Division(string firstNumber, string secondNumber)
+        {
+            if (IsNumeric(firstNumber) && IsNumeric(secondNumber))
+            {
+                var division = ConvertToDecimal(firstNumber) / ConvertToDecimal(secondNumber); 
+
+                return Ok(division.ToString());
+            }
+            return BadRequest("Invalid Input");
+
+        }
+
+        //==========================================================
+
+        [HttpGet("Subtratcion/{firstNumber}/{secondNumber}")] 
+
+        // aqui sera a subtração
+        public IActionResult Subtratcion(string firstNumber, string secondNumber)
+        {
+            if (IsNumeric(firstNumber) && IsNumeric(secondNumber))
+            {
+                var Subtratcion = ConvertToDecimal(firstNumber) - ConvertToDecimal(secondNumber); 
+
+                return Ok(Subtratcion.ToString());
+            }
+            return BadRequest("Invalid Input"); 
+
+        }
+
+        //==========================================================
+
+
+        [HttpGet("Multiplication/{firstNumber}/{secondNumber}")] 
+        
+        //aqui sera a multiplicação
+        public IActionResult Multiplication(string firstNumber, string secondNumber) 
+        {
+            if (IsNumeric(firstNumber) && IsNumeric(secondNumber))
+            {
+                var Multiplication = ConvertToDecimal(firstNumber) * ConvertToDecimal(secondNumber);
+                return Ok(Multiplication.ToString());
+            }
+            return BadRequest("Invalid Input"); 
+
+
+        }
+
+        //==========================================================
 
         private bool IsNumeric(string strtNumber) //renomeei para strNumber
         {
@@ -76,6 +164,7 @@ namespace RestWithASP_NET5Udemy.Controllers
 
             throw new NotImplementedException();
         }
+        //==========================================================
 
         private decimal ConvertToDecimal(string strtNumber) //renomeei para strNumber e mudei para decimal
         {
